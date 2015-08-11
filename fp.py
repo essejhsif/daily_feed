da344deacc52fb5bb74f38a115c9a45f133cd404
Jesse Fish <jessefish@gmail.com>
2015-08-10 23:44:58 -0400

    modified xml output

--- fp.py
+++ fp.py
@@ -5,9 +5,9 @@ from xml.etree.ElementTree import Element, SubElement, Comment
 import xmlformatter
 
 root = ET.Element("rss")   
-item = ET.SubElement(root,"item")
+#item = ET.SubElement(root,"item")
 
-datatau = ET.SubElement(item,"datatau")
+datatau = ET.SubElement(root,"datatau")
 d = feedparser.parse('http://www.datatau.com/rss')
 
 for f in d['entries']:
@@ -19,10 +19,11 @@ for f in d['entries']:
         link = filter(lambda x: x in string.printable, str(f['link']).encode('utf-8').replace(",",""))
     except:
         link = ""
-    ET.SubElement(datatau,"title").text = str(title)
-    ET.SubElement(datatau,"link").text = str(link)
+    item = ET.SubElement(datatau,"item")
+    ET.SubElement(item,"title").text = str(title)
+    ET.SubElement(item,"link").text = str(link)
     
-dataisbeautiful = ET.SubElement(item,"dataisbeautiful")
+dataisbeautiful = ET.SubElement(root,"dataisbeautiful")
 d = feedparser.parse('https://www.reddit.com/r/dataisbeautiful/.rss')
 
 for f in d['entries']:
@@ -34,10 +35,11 @@ for f in d['entries']:
         link = filter(lambda x: x in string.printable, str(f['link']).encode('utf-8').replace(",",""))
     except:
         link = ""
-    ET.SubElement(dataisbeautiful,"title").text = str(title)
-    ET.SubElement(dataisbeautiful,"link").text = str(link)
+    item = ET.SubElement(dataisbeautiful,"item")
+    ET.SubElement(item,"title").text = str(title)
+    ET.SubElement(item,"link").text = str(link)
 
-hacking = ET.SubElement(item,"hacking")
+hacking = ET.SubElement(root,"hacking")
 d = feedparser.parse('https://www.reddit.com/r/hacking/.rss')
 
 for f in d['entries']:
@@ -49,10 +51,11 @@ for f in d['entries']:
         link = filter(lambda x: x in string.printable, str(f['link']).encode('utf-8').replace(",",""))
     except:
         link = ""
-    ET.SubElement(hacking,"title").text = str(title)
-    ET.SubElement(hacking,"link").text = str(link)
+    item = ET.SubElement(hacking,"item")
+    ET.SubElement(item,"title").text = str(title)
+    ET.SubElement(item,"link").text = str(link)
 
-cybersecurity = ET.SubElement(item,"cybersecurity")  
+cybersecurity = ET.SubElement(root,"cybersecurity")  
 d = feedparser.parse('https://www.reddit.com/r/cybersecurity/.rss')
 
 for f in d['entries']:
@@ -64,10 +67,11 @@ for f in d['entries']:
         link = filter(lambda x: x in string.printable, str(f['link']).encode('utf-8').replace(",",""))
     except:
         link = ""
-    ET.SubElement(cybersecurity,"title").text = str(title)
-    ET.SubElement(cybersecurity,"link").text = str(link)
+    item = ET.SubElement(cybersecurity,"item")
+    ET.SubElement(item,"title").text = str(title)
+    ET.SubElement(item,"link").text = str(link)
 
-ycombinator = ET.SubElement(item,"ycombinator")  
+ycombinator = ET.SubElement(root,"ycombinator")  
 d = feedparser.parse('https://news.ycombinator.com/rss')
 
 for f in d['entries']:
@@ -79,8 +83,9 @@ for f in d['entries']:
         link = filter(lambda x: x in string.printable, str(f['link']).encode('utf-8').replace(",",""))
     except:
         link = ""
-    ET.SubElement(ycombinator,"title").text = str(title)
-    ET.SubElement(ycombinator,"link").text = str(link)
+    item = ET.SubElement(ycombinator,"item")
+    ET.SubElement(item,"title").text = str(title)
+    ET.SubElement(item,"link").text = str(link)
 
 file = open("daily_news.xml","w")
 ET.ElementTree(root).write(file)
