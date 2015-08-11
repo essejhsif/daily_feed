4984dd61d48dabd7a2c27fd19457c0a559a6dbf3
Jesse Fish <jessefish@gmail.com>
2015-08-10 23:45:18 -0400

    older output of daily feed

---
+++ fp_old.py
@@ -0,0 +1,35 @@
+import feedparser
+
+d = feedparser.parse('http://www.datatau.com/rss')
+
+print "datatau"
+for f in d['entries']:
+  print f['title'] + " - " + f['link'] 
+
+d = feedparser.parse('https://www.reddit.com/r/dataisbeautiful/.rss')
+
+print "\n/r/dataisbeautiful"
+for f in d['entries']:
+  print f['title'] + " - " + f['link']
+
+d = feedparser.parse('https://www.reddit.com/r/arduino/.rss')
+
+print "\n/r/arduino"
+for f in d['entries']:
+  print f['title'] + " - " + f['link']
+
+d = feedparser.parse('https://news.ycombinator.com/rss')
+
+print "\nhacker news"
+for f in d['entries']:
+  print f['title'] + " - " + f['link']
+
+d = feedparser.parse('http://what.thedailywtf.com/latest.rss')
+
+print "\nthe daily wtf - forums"
+for f in d['entries']:
+  print f['title'] + " - " + f['link']
+
+
+
+
